2b93cdee31f4f5244b3f31a583466e59fd044c2c
wcampbell <wcampbell1995@gmail.com>
2023-09-03 01:01:22 -0400

    Fix ensure_no_std

--- ensure_no_std/src/bin/main.rs
+++ ensure_no_std/src/bin/main.rs
@@ -1,27 +1,39 @@
-//! Based on https://github.com/rustwasm/wee_alloc/tree/master/example
-//! Run with `cargo +nightly run --release`
-
+//! cargo build --target thumbv7em-none-eabihf
 #![no_std]
 #![no_main]
-#![feature(core_intrinsics, alloc_error_handler)]
+
+extern crate alloc;
 
 use adsb_deku::Frame;
 use hexlit::hex;
 use rsadsb_common::Airplanes;
 
-extern crate alloc;
-extern crate wee_alloc;
+use core::panic::PanicInfo;
 
-#[no_mangle]
-#[allow(non_snake_case)]
-fn _Unwind_Resume() {}
+use cortex_m_rt::entry;
+use embedded_alloc::Heap;
 
 #[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+static HEAP: Heap = Heap::empty();
 
-#[no_mangle]
-pub extern "C" fn main() {
+#[entry]
+fn main() -> ! {
+    // Initialize the allocator BEFORE you use it
+    {
+        use core::mem::MaybeUninit;
+        const HEAP_SIZE: usize = 1024;
+        static mut HEAP_MEM: [MaybeUninit<u8>; HEAP_SIZE] = [MaybeUninit::uninit(); HEAP_SIZE];
+        unsafe { HEAP.init(HEAP_MEM.as_ptr() as usize, HEAP_SIZE) }
+    }
+
     let buffer = hex!("8da7c32758ab75f3291315f10261");
     let _ = Frame::from_reader(&buffer[..]).unwrap();
     let _ = Airplanes::new();
+
+    loop { /* .. */ }
+}
+
+#[panic_handler]
+fn panic(_: &PanicInfo) -> ! {
+    loop {}
 }
