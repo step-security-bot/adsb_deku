5a85d064cb9834e388e69a1cadde2ec9dd638257
wcampbell <wcampbell1995@gmail.com>
2023-09-03 00:54:03 -0400

    fixup! wip: Update deku

--- libadsb_deku/tests/file.rs
+++ libadsb_deku/tests/file.rs
@@ -6,12 +6,10 @@ const TEST_STR: &str = include_str!("../tests/lax-messages.txt");
 
 #[test]
 fn lax_messages() {
-    env_logger::init();
     // Read from test file and assert display implemented and non panic decode
     for line in TEST_STR.lines() {
         let len = line.chars().count();
         let hex = &mut line.to_string()[1..len - 1].to_string();
-        log::info!("{hex}");
         let bytes = hex::decode(&hex).unwrap();
         // test non panic decode
         let cursor = Cursor::new(bytes);
